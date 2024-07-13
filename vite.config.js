6323ec197f732d213d271e3ea433a97c37c0d176
Rutheesh <rutheeshreddychittepu@gmail.com>
2024-07-13 12:38:11 +0530

    gh-pages now set up

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/YMC-Inventure-page",
   plugins: [react()],
-  build: {
-    outDir: 'build', // Specify the output directory
-  },
 })
